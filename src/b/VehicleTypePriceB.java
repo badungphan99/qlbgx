3c19221e331fe39e3da75cc0f042ed2604b26c33
badungphan99 <badungphan99@gmail.com>
2018-11-18 00:27:55 +0700

    lưu may cai moi code lên khong co gi dau

--- src/b/VehicleTypePriceB.java
+++ src/b/VehicleTypePriceB.java
@@ -11,14 +11,5 @@ public class VehicleTypePriceB {
 
 
     //Insert các cài đặt cho mỗi loại xe
-    public void insertBicycleSetting (int price, int parkingPerios, int overdue, int perHour, int perDay){
-        String sql = "UPDATE vehicle_type_price SET price = ?";
-        switch (overdue){
-            case 1:
-                sql = sql + ", overdue = 1 WHERE id = 1";
-                break;
-            case 2:
-               // sql = sql + ", parking_perios = ?, overdue = 2, "
-        }
-    }
+
 }
--- src/da/VehicleTypePriceDA.java
+++ src/da/VehicleTypePriceDA.java
@@ -1,6 +1,7 @@
 package da;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class VehicleTypePriceDA {
@@ -14,6 +15,30 @@ public class VehicleTypePriceDA {
             e.printStackTrace();
         } catch (SQLException e) {
             // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void insertBicycleSetting (int price, int parkingPerios, int overdue, int perHour, int perDay){
+        String sql = "UPDATE vehicle_type_price SET price = ?";
+        switch (overdue){
+            case 1:
+                sql = sql + ", overdue = 1 WHERE id = 1";
+                break;
+            case 2:
+                sql = sql + ", parking_perios = ?, overdue = 2, per_hour = ? WHERE id = 1";
+                break;
+            case 3:
+                sql = sql + ", parking_perios = ?, overdue = 2, per_day = ? WHERE id = 1";
+                break;
+            case 4:
+                sql = sql + ", parking_perios = ?, overdue = 2, per_hour = ?, per_day = ? WHERE id = 1";
+                break;
+        }
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1,price);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
