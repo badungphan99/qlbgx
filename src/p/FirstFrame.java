c82ebbd5c54113ee4543fb88de64731b88a1276b
nhathiu <phamnhathieu_t62@hus.edu.vn>
2018-11-28 17:39:36 +0700

    sua phong nen

--- src/p/FirstFrame.java
+++ src/p/FirstFrame.java
@@ -32,25 +32,26 @@ public class FirstFrame extends JFrame{
 
     public FirstFrame() {
         this.setTitle("Hệ thống quản lý bãi gửi xe");
-        this.setSize(1024, 768);
+        this.setSize(1024, 760);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         contentPane = new JPanel();
         panel = new JPanel();
         this.setContentPane(contentPane);
-        contentPane.setBackground(Color.decode("#87CEEB"));
         contentPane.setBackground(Color.WHITE);
-        nameSystem = new JLabel("HỆ THỐNG QUẢN LÝ BÃI GỬI XE");
+        //this.setResizable(false);
+       /* nameSystem = new JLabel("HỆ THỐNG QUẢN LÝ BÃI GỬI XE");
         Font font = new Font("Georgia", Font.BOLD, 20);
         nameSystem.setFont(font);
         nameSystem.setVerticalAlignment(SwingConstants.CENTER);
-        nameSystem.setForeground(Color.black);
-        contentPane.add(nameSystem);
+        nameSystem.setForeground(Color.black)*/;
+       // contentPane.add(nameSystem);
 
-        btnLogin = new JButton("Login");
-        contentPane.add(btnLogin);
+        btnLogin = new JButton();
+        btnLogin.setSize(1024,760);
+       // contentPane.add(btnLogin);
         try {
-            BufferedImage images = ImageIO.read(new File("image/icon.jpg"));
-            ImageIcon icons = new ImageIcon(images.getScaledInstance(20,20,images.SCALE_SMOOTH));
+            BufferedImage images = ImageIO.read(new File("image/baixe3.jpg"));
+            ImageIcon icons = new ImageIcon(images.getScaledInstance(1024,768,images.SCALE_SMOOTH));
             btnLogin.setIcon(icons);
         } catch (IOException e) {
             e.printStackTrace();
@@ -66,18 +67,10 @@ public class FirstFrame extends JFrame{
 
             }
         });
-       label = new JLabel();
-       label.setSize(1000,300);
-        try {
-            BufferedImage image = ImageIO.read(new File("image/baixe3.jpg"));
-            ImageIcon icon = new ImageIcon(image.getScaledInstance(1490,720,image.SCALE_SMOOTH));
-            label.setIcon(icon);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        panel.setBackground(Color.WHITE);
-        panel.add(label);
-        contentPane.add(panel, BorderLayout.SOUTH);
+
+      contentPane.setLayout(new BorderLayout());
+      contentPane.add(btnLogin, BorderLayout.PAGE_START);
+      this.setResizable(false);
     }
 
 
